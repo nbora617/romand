34336d00f0a2ad79f1858742d8fb801ea01cfd8b
bora <nbora617@naver.com>
2023-08-29 18:12:05 +0900

    last

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer>
+        <div class="f_top">
+            <div class="left">
+                <a href="#">이용약관동의</a>
+                <a href="#">개인정보처리방침</a>
+                <a href="#">영상정보처리방침</a>
+                <a href="#">마케팅/제휴 문의</a>
+                <a href="#">해외 수출 문의</a>
+                <a href="#">고객센터</a>
+            </div>
+            <div class="right">
+                <a href="#"><img src="./image/Youtube.png" alt="유튜브"></a>
+                <a href="#"><img src="./image/Instagram.png" alt="인스타그램"></a>
+                <a href="#"><img src="./image/blog.png" alt="블로그"></a>
+                <a href="#"><img src="./image/facebook.png" alt="페이스북"></a>
+                <a href="#"><img src="./image/sns_kakao.png" alt="카카오톡"></a>
+            </div>
+        </div>
+        <div class="f_btm">
+            <div class="left">
+                <h1><a href=""><img src="./image/logo_dark.png" alt="롬앤"></a></h1>
+                <address>
+                    (주)아이패밀리SC<br>
+                    서울특별시 송파구 동남로 122(문정동) COLLECTED 빌딩 Tel : 02.1670.2238 Fax : 02.6910.4111 사업자등록번호 : 120-86-00633통신판매신고번호 : 제2022-서울송파-1690호 대표자 : 김태욱,김성현
+                </address>
+                <p>&copy; 2017 IFAMILY SC, CO.,LTD.All Right Reserved.</p>
+            </div>
+            <div class="right">
+                <p>롬앤 고객센터 이용안내</p>
+                <a href="tel:16702238" class="tel">1670-2238</a>
+                <p class="info">Mon-Fri. 10:00 - 17:00 (주말 및 공휴일 휴무)<br>
+                    Lunch. 12:20 - 13:30</p>
+                <a href="#">1:1 상담작성 <i class="fa-solid fa-angle-right"></i></a>
+            </div>
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,142 @@
+<header>
+        <div class="head_bg">
+            <h1><a href="index.html"><img src="./image/logo.png" alt="롬앤"></a></h1>
+            <nav>
+                <ul>
+                    <li><a href="#">BRAND</a>
+                        <ul class="sub">
+                            <li><a href="#">롬앤 소개</a></li>
+                            <li><a href="#">롬앤 소식</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="product_list.html">SHOP</a>
+                        <ul class="sub">
+                            <li><a href="product_list.html">전체</a></li>
+                            <li><a href="product_list.html">BASE</a></li>
+                            <li><a href="product_list.html">EYE</a></li>
+                            <li><a href="product_list.html">LIP</a></li>
+                            <li><a href="product_list.html">NAIL</a></li>
+                            <li><a href="product_list.html">TOOL</a></li>
+                            <li><a href="product_list.html">SET</a></li>
+                            <li><a href="product_list.html">EDITION</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">LOOK</a>
+                        <ul class="sub">
+                            <li><a href="#">룩튜토리얼</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">EVENT</a>
+                        <ul class="sub">
+                            <li><a href="#">진행중</a></li>
+                            <li><a href="#">종료</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">COMMUNITY</a>
+                        <ul class="sub">
+                            <li><a href="#">NOTICE</a></li>
+                            <li><a href="#">REVIEW</a></li>
+                            <li><a href="#">Q&A</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">PROJECT</a>
+                        <ul class="sub">
+                            <li><a href="#">케미프로젝트</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <div class="gnb">
+                <a href="#" class="searchGnb"><img src="./image/search.png" alt="검색"></a>
+                <a href="login.html" class="user"><img src="./image/User.png" aria-hidden="true" alt="로그인"></a>
+                <a href="#" class="cart"><img src="./image/cart.png" aria-hidden="true" alt="장바구니"></a>
+                <div id="all_m_menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div> 
+            </div>
+        </div>
+        <!-- open search -->
+        <div class="open_search">
+            <form action="#" id="searchFrm" method="get">
+                <fieldset>
+                    <legend class="skip">검색</legend>
+                    <div class="user_text">
+                        <img src="./image/search.png" alt="">
+                        <input type="text" id="search" placeholder="검색어를 입력해주세요">
+                        <button type="reset" id="search_close"><img src="./image/close2.png" alt=""></button>
+                    </div>
+                    <button type="button" id="searchBtn">검색</button>
+                </fieldset>
+                <div class="keyword">
+                    <h3>추천 검색어</h3>
+                    <div class="right">
+                        <a href="#">#듀이풀 워터틴트</a>
+                        <a href="#">#블룸 인 커버핏 쿠션</a>
+                        <a href="#">#백미 톤업 크림</a>
+                        <a href="#">#트윙크 팬 라이너</a>
+                        <a href="#">#한올 쉐이드 라이너</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- open_m_menu -->
+        <div class="open_m_menu">
+            <h1><a href="#"><img src="./image/logo_dark.png" alt="롬앤"></a></h1>
+            <button type="button" id="close"><img src="./image/close.png" alt="닫기"></button>
+            <ul class="m_menu">
+                <li>
+                    <a href="#"><span>BRAND</span><i class="fa-solid fa-chevron-right fa-rotate-90"></i></a>
+                    <ul class="sub_menu">
+                        <li><a href="#">롬앤 소개</a></li>
+                        <li><a href="#">롬앤 소식</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="product_list.html"><span>SHOP</span><i class="fa-solid fa-chevron-right fa-rotate-90"></i></a>
+                    <ul class="sub_menu">
+                        <li><a href="product_list.html">전체</a></li>
+                        <li><a href="product_list.html">BASE</a></li>
+                        <li><a href="product_list.html">EYE</a></li>
+                        <li><a href="product_list.html">LIP</a></li>
+                        <li><a href="product_list.html">NAIL</a></li>
+                        <li><a href="product_list.html">TOOL</a></li>
+                        <li><a href="product_list.html">SET</a></li>
+                        <li><a href="product_list.html">EDITION</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><span>LOOK</span><i class="fa-solid fa-chevron-right fa-rotate-90"></i></a>
+                    <ul class="sub_menu">
+                        <li><a href="#">룩튜토리얼</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><span>EVENT</span><i class="fa-solid fa-chevron-right fa-rotate-90"></i></a>
+                    <ul class="sub_menu">
+                        <li><a href="#">진행중</a></li>
+                        <li><a href="#">종료</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><span>COMMUNITY</span><i class="fa-solid fa-chevron-right fa-rotate-90"></i></a>
+                    <ul class="sub_menu">
+                        <li><a href="#">NOTICE</a></li>
+                        <li><a href="#">REVIEW</a></li>
+                        <li><a href="#">Q&A</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><span>PROJECT</span><i class="fa-solid fa-chevron-right fa-rotate-90"></i></a>
+                    <ul class="sub_menu">
+                        <li><a href="#">케미프로젝트</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="m_menu_gnb">
+                <a href="#">MY PAGE</a>
+                <a href="#">고객센터</a>
+            </div>
+        </div>
+    </header>
